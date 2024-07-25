3e9890f69aac042f295782066a82a216f7e708b4
RodionKipitok <kucernukrabota@gmail.com>
2024-07-24 18:07:50 -0700

    make mobile menu

--- src/components/NavBar/Header.js
+++ src/components/NavBar/Header.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react';
 import '../NavBar/style.css';
 import NavigationLink from '../NavBar/NavigationLink';
-import { ReactComponent as MobileMenu } from '../../assets/img/burger-menu-svgrepo-com.svg';
 import React from 'react';
 
 const NavBar = () => {
@@ -29,18 +28,22 @@ const NavBar = () => {
                <a href="/" className="nav-logo">
                   <strong>Radion Kuchernuk</strong>
                </a>
-               <nav className={` nav ${isOpen ? 'active' : ''}`}>
+               <nav className={`nav ${isOpen ? 'open' : ''}`}>
                   <NavigationLink />
+               </nav>
+               <div className={`burger ${isOpen ? 'active' : ''}`}>
                   <button
+                     className="burger-button"
                      type="button"
-                     className="nav__btn-mobile-menu--close"
                      onClick={() => {
                         setIsOpen(!isOpen);
                      }}
                   >
-                     close
+                     <span className="burger-line"></span>
+                     <span className="burger-line"></span>
+                     <span className="burger-line"></span>
                   </button>
-               </nav>
+               </div>
             </div>
          </div>
       </header>
@@ -49,37 +52,6 @@ const NavBar = () => {
 
 export default NavBar;
 
-{
-   /* <header className={` header ${scrolled ? 'scrolled' : ''}`}>
-<div className="container">
-  <div className="wrapper-modile-menu-logo-btnClose">
-    <a href="/" className="nav-logo">
-      <strong>Radion Kuchernuk</strong>
-    </a>
+// className={`nav ${isOpen ? 'open' : ''}`}
 
-    <nav className={` nav ${isOpen ? 'active' : ''}`}>
-      <NavigationLink />
-      <button
-        type="button="
-        className="nav__btn-mobile-menu--close"
-        onClick={() => {
-          setIsOpen(!isOpen);
-        }}
-      >
-        close
-      </button>
-    </nav>
-
-    <button
-      type="button"
-      className="nav__btn-mobile-menu--open"
-      onClick={() => {
-        setIsOpen(!isOpen);
-      }}
-    >
-      <MobileMenu className="icon-btn-menu" />
-    </button>
-  </div>
-</div>
-</header> */
-}
+// className={`burger ${isOpen ? 'active' : ''}`}
--- src/components/NavBar/SocialLink.js
+++ src/components/NavBar/SocialLink.js
@@ -2,19 +2,19 @@ import navIcon1 from '../../assets/img/nav-icon1.svg';
 import navIcon2 from '../../assets/img/nav-icon2.svg';
 import navIcon3 from '../../assets/img/nav-icon3.svg';
 const SocialLink = () => {
-  return (
-    <div className="social-icon">
-      <a href="/">
-        <img src={navIcon1} alt="" />
-      </a>
-      <a href="/">
-        <img src={navIcon2} alt="" />
-      </a>
-      <a href="/">
-        <img src={navIcon3} alt="" />
-      </a>
-    </div>
-  );
+   return (
+      <div className="social-icon">
+         <a href="https://ua.linkedin.com/">
+            <img src={navIcon1} alt="link-linkedin" />
+         </a>
+         <a href="https://x.com">
+            <img src={navIcon2} alt="link-x" />
+         </a>
+         <a href="https://www.instagram.com/">
+            <img src={navIcon3} alt="link-instagram" />
+         </a>
+      </div>
+   );
 };
 
 export default SocialLink;
