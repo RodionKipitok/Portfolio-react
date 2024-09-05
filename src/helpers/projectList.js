3eb15d15c3ca356a119aa42703a3c9cea4894e24
RodionKipitok <kucernukrabota@gmail.com>
2024-09-05 08:34:58 +0300

    added projectList

---
+++ src/helpers/projectList.js
@@ -0,0 +1,63 @@
+import freelancerLandingPage from '../assets/project/rodionkipitok.github.io_Portfolio_.png';
+import webStudio from '../assets/project/webStudio.png';
+import projImg2 from '../assets/img/project-img2.png';
+import projImg3 from '../assets/img/project-img3.png';
+
+const projects = [
+   {
+      id: 1,
+      title: 'Freelancer landing page',
+      description: 'Development',
+      imgUrl: freelancerLandingPage,
+      technologies: ['CSS3', 'HTML5', 'JavaScript'],
+      linkCodeGitHub: 'https://github.com/RodionKipitok/Portfolio',
+      linkGitHubPage: 'https://rodionkipitok.github.io/Portfolio/index.html',
+   },
+   {
+      id: 2,
+      title: 'Web Studio landing page',
+      description: 'Development',
+      imgUrl: webStudio,
+      technologies: ['CSS3', 'HTML5', 'JavaScript'],
+      linkCodeGitHub: 'https://github.com/RodionKipitok/Web-Studio',
+      linkGitHubPage: 'https://rodionkipitok.github.io/Web-Studio/',
+   },
+   {
+      id: 3,
+      title: 'Business Startup',
+      description: 'Design & Development',
+      imgUrl: projImg3,
+      technologies: '',
+      linkCodeGitHub: '',
+      linkGitHubPage: '',
+   },
+   {
+      id: 4,
+      title: 'Business Startup',
+      description: 'Design & Development',
+      imgUrl: projImg3,
+      technologies: '',
+      linkCodeGitHub: '',
+      linkGitHubPage: '',
+   },
+   {
+      id: 5,
+      title: 'Business Startup',
+      description: 'Design & Development',
+      imgUrl: projImg2,
+      technologies: '',
+      linkCodeGitHub: '',
+      linkGitHubPage: '',
+   },
+   {
+      id: 6,
+      title: 'Business Startup',
+      description: 'Design & Development',
+      imgUrl: projImg3,
+      technologies: '',
+      linkCodeGitHub: '',
+      linkGitHubPage: '',
+   },
+];
+
+export default projects;
