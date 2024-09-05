467bac3df4ab6a2b749f2ca3aca7105c23162a02
RodionKipitok <kucernukrabota@gmail.com>
2024-09-05 17:06:48 +0300

    update css footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,7 +12,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 export const App = () => {
    return (
-      <div>
+      <div className='page'>
          <Router>
             <ScrollToTop />
             <NavBar />
